6520574fcd046f0e729b8aaf6893e0e23c60a57e
Stoycho Velev <stoycho.velev@Stoycho-Velev.local>
2023-06-24 15:27:11 +0300

    new stuff

---
+++ advanced/unit_converter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Temperature and Metric Converter")
--- beginner/palindrome.py
+++ beginner/palindrome.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 word = str(input("Give word and I tell:\n"))
-
-word = word.replace(" ","")
-
 word = word.lower()
-reversed_word = ''.join(reversed(word))
 
-if word == reversed_word:
+if word == word[::-1]:
 	print(f"The word {word} is a palindrome.")
 else:
 	print(f"The word {word} is NOT a palindrome")
